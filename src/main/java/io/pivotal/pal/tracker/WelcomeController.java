dd5564cf9df8d2df77ca521adf5ae0ac1eddda73
thirumalasabbi <lakshmi.tirumala92@gmail.com>
2020-05-27 05:05:28 +0100

    Pipeline 1.1

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -6,8 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
-    @Value("${welcome.message}")
+
     String message;
+
+    public WelcomeController(@Value("${welcome.message}") String msg) {
+        message = msg;
+    }
 
     @GetMapping("/")
     public String sayHello() {
